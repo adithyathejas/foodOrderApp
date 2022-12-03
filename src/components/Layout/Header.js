b1271aa90d52eafb609a6f2de590179cd723e2c5
adithyathejas <adithyathejus00@gmail.com>
2022-12-03 12:38:15 +0530

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+
+import './Header.css'
+
+const Header = props => {
+    return <>
+    <header className='header'>
+        <h1>React Meals</h1>
+        <HeaderCartButton/>
+    </header>
+    <div>
+        <img src={mealsImage}/>
+    </div>
+    </> 
+
+}
+
+export default Header; 
