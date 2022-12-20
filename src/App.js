85f6e4888ba72033fe15b554ef7303bf562726c1
adithyathejas <adithyathejus00@gmail.com>
2022-12-20 15:10:15 +0530

    created static cart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
 import Meals from "./Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <>
+     <Cart/>
       <Header></Header>
       <main>
         <Meals></Meals>
